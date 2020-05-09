fb601a3c45e3f3ccb39fcc757bf5bd01231d1c86
wensl3y <45434530+wensl3y@users.noreply.github.com>
2020-05-09 17:10:45 +0400

    Add files via upload

---
+++ dabest.py
@@ -0,0 +1,27 @@
+# Based on 'THE BEST' meme. Original song: Foo Fighters - The Best Of You.
+# The full length - 4 minutes 17 seconds.
+
+from time import sleep
+
+print('I got another confession to make')
+sleep(5)
+print("I'm your fool")
+sleep(3)
+print("Everyone's got their chains to break")
+sleep(4)
+print("Holdin' you")
+sleep(4)
+print('Where you born to resist\nOr be abused?')
+sleep(8)
+print('Is someone getting')
+
+i = 0
+while i != 246:
+    print('THE BEST')
+    sleep(1)
+    i += 1
+
+sleep(1)
+
+if i == 246:
+    print('OF YOU')
